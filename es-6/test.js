382a61c3a519412428e051c5d6cd1f333dc21133
mamunsj <mamunsj2616@gmail.com>
2023-02-20 09:10:48 +0600

    changed the directory name

---
+++ es-6/test.js
@@ -0,0 +1,21 @@
+const test = 'it works';
+
+const calcAge = age => {
+  const currentAge = age - 1999;
+  return currentAge;
+};
+console.log(calcAge(2023));
+console.log('');
+console.log('Kemo');
+console.log('');
+console.log('');
+console.log('');
+console.log();
+console.log('chldsf');
+console.lo;
+console.log('$');
+console.log();
+console.log();
+console.log();
+console.log();
+console.log(ldlfd);
